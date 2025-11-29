6003d8c2686afd8dcab4c5d921b82d3d7781de10
gigimko <nowyay123456@gmail.com>
2025-11-29 08:41:01 -0500

    ,k

--- main.py
+++ main.py
@@ -21,13 +21,43 @@ class Node():
             self.childLeft.display()
             
         print(self.data)
+        
         if self.childRight != None:
             self.childRight.display()
-            
+
+    def binarysearch(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.childLeft is not None:
+                return self.childLeft.binarysearch(val)
+            else:
+                return False
+        
+
+        else:
+            if self.childRight is not None:
+                return self.childRight.binarysearch(val)
+            else:
+                return False
+
+    def minmax(self):
+        min_node = self
+        while min_node.childLeft is not None:
+            min_node = min_node.childLeft
+        
+        max_node = self
+        while max_node.childRight is not None:
+            max_node = max_node.childRight
+        
+        return (min_node.data, max_node.data)
+
 
 root = Node(100)
 root.add(33)
 root.add(50)
 root.add(99)
 root.add(20)
-root.display()
+print(root.binarysearch(444))
+print(root.minmax())
